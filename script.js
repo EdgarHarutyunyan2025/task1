beb2854a37fda17ae8cda821438e7074ce7479c7
edgar <edgar_harutyunyan_2025@mail.ru>
2024-11-22 11:12:26 +0400

    task2

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 async function delayedAddition(a, b) {
-  console.log('number1...5');
-  console.log('number2...3');
+  console.log('number1 = 5');
+  console.log('number2 = 3');
   await new Promise(resolve => setTimeout(resolve, 2000)); // 2 секунды
   const result = a + b;
-  console.log(`sum = : ${result}`);
+  console.log(`sum =  ${result}`);
 }
 
 delayedAddition(5, 3);
